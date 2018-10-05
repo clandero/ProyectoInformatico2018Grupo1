22fa44c2121cb6224c2a2fd552de4b969fdd1bc6
arken <arken@arken-System-Product-Name>
2018-10-04 21:58:59 -0300

    FileUpload no full path

--- ProyectoInformatico2018/src/java/Controlador/FileUploadServlet.java
+++ ProyectoInformatico2018/src/java/Controlador/FileUploadServlet.java
@@ -73,6 +73,7 @@ public class FileUploadServlet extends HttpServlet {
         writer.println("New file " + fileName + " created at " + path);
         LOGGER.log(Level.INFO, "File{0}being uploaded to {1}", 
                 new Object[]{fileName, path});
+        userDao.upFile(mail, tema, fileName, "/docs/"+fileName);
     } catch (FileNotFoundException fne) {
         writer.println("You either did not specify a file to upload or are "
                 + "trying to upload a file to a protected or nonexistent "
@@ -92,7 +93,7 @@ public class FileUploadServlet extends HttpServlet {
             writer.close();
         }
     }
-    userDao.upFile(mail, tema, fileName, path+fileName);
+    
 }
 
 private String getFileName(final Part part) {
