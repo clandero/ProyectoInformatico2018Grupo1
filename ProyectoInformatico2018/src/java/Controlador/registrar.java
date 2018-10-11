502f906d15625cd53fae1d24e618bb95a51fd963
berko <berko@berko-HP-245-G5-Notebook-PC>
2018-10-11 16:15:13 -0300

    función de registro dentro del DAO Usuario

--- ProyectoInformatico2018/src/java/Controlador/registrar.java
+++ ProyectoInformatico2018/src/java/Controlador/registrar.java
@@ -29,12 +29,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class registrar extends HttpServlet {
 
-    Connection con;
-    Statement st;
-    String nombre, correo, pass, depa, tipo;
-    String urlbd = "jdbc:postgresql://bdd.inf.udec.cl/pigrupo1";
-    String userbd = "pigrupo1";
-    String passwordbd = "pigrupo1";
+    private static UsuarioDao userDao = new UsuarioDao();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -51,26 +46,19 @@ public class registrar extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset-UTF-8");
-        nombre = request.getParameter("txtNombre");
-        correo = request.getParameter("txtCorreo");
-        pass = request.getParameter("txtPassword");
+        String nombre = request.getParameter("txtNombre");
+        String correo = request.getParameter("txtCorreo");
+        String pass = request.getParameter("txtPassword");
         String passencript = DigestUtils.md5Hex(pass);
-        depa = request.getParameter("radio");
-        tipo = request.getParameter("search_categories");
+        String depa = request.getParameter("radio");
+        String tipo = request.getParameter("search_categories");
+        Usuario u1 = new Usuario(nombre, correo, pass, depa, tipo);
         try {
-            Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection(urlbd, userbd, passwordbd);
-            st = con.createStatement();
-            String query = "insert into plataforma_colaborativa.usuario values ('" + correo + "','" + nombre + "','" + tipo + "','" + depa + "','" + passencript + "');";
-            st.execute(query);
-            //  System.out.println("datos insertados");
-            System.out.println(query);
-
+            userDao.save(u1);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("datos no insertados");
         }
-        Usuario u1 = new Usuario(nombre, correo, pass, depa, tipo);
         request.getSession().setAttribute("usuario1", u1);
         request.getRequestDispatcher("perfil.jsp").forward(request, response);
     }
