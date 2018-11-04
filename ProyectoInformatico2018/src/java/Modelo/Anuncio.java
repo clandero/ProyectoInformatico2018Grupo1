d4878892b0411dc7ffb465d0816529bf8234802a
vanessarriagada <vanessarriagada@udec.cl>
2018-11-04 19:31:59 -0300

    Se añadió el campo titulo y se modifico el constructor.

--- ProyectoInformatico2018/src/java/Modelo/Anuncio.java
+++ ProyectoInformatico2018/src/java/Modelo/Anuncio.java
@@ -3,12 +3,14 @@ package Modelo;
 public class Anuncio {
     private int n_anun;
     private String contenido;
+    private String titulo;
+    private String fecha_anuncio;
     
     public Anuncio(){
     }
     
-    public Anuncio(int n, String cont){
-        this.n_anun = n;
+    public Anuncio(String titulo, String cont){
+        this.titulo = titulo;
         this.contenido = cont;
     }
     
@@ -23,5 +25,30 @@ public class Anuncio {
     }
     public int getNumero(){
         return this.n_anun;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getFecha_anuncio() {
+        return fecha_anuncio;
+    }
+
+    public void setFecha_anuncio(String fecha_anuncio) {
+        this.fecha_anuncio = fecha_anuncio;
+    }
+    
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getN_anun() {
+        return n_anun;
+    }
+
+    public void setN_anun(int n_anun) {
+        this.n_anun = n_anun;
     }
 }
